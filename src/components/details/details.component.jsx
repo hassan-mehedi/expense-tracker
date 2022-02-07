e11480b96eb30ac443a688853cfdd1995de09e01
Mehedi Hassan <howlader.mehedihassan@gmail.com>
2022-02-07 23:21:38 +0600

    feat: structure the app

--- src/components/details/details.component.jsx
+++ src/components/details/details.component.jsx
@@ -1,30 +1,38 @@
 import React, { useState } from "react";
+import "./details.style.scss";
 
 export default function Details({ user, addCash, removeCash }) {
     const [money, setMoney] = useState(0);
+
     const cashIn = {
         type: "cash_in",
         money: money,
         date: new Date(),
     };
-
     const cashOut = {
         type: "cash_out",
         money: money,
         date: new Date(),
     };
+    console.log(user);
     return (
         <div className="details">
-            <input type="number" onChange={(event) => setMoney(event.target.value)} />
+            <h1>Name: {user.name}</h1>
+            <input type="number" onChange={(event) => setMoney(event.target.value)} value={money} />
             <button onClick={() => addCash(cashIn, user.id)}>Cash In</button>
             <button onClick={() => addCash(cashOut, user.id)}>Cash Out</button>
-            <h1>Name: {user.name}</h1>
             <ul>
                 {user.cash &&
                     user.cash.map((cash, index) => (
                         <li key={index}>
                             <p>{cash.type}</p>
                             <p>{cash.money}</p>
+                            <p>{new Date(cash.date.seconds * 1000 + cash.date.nanoseconds).toLocaleDateString()}</p>
+                            <p>
+                                {new Date(cash.date.seconds * 1000 + cash.date.nanoseconds)
+                                    .toLocaleTimeString()
+                                    .slice(0, 8)}
+                            </p>
                             <button onClick={(event) => removeCash(cash, user.id)}>Remove</button>
                         </li>
                     ))}
